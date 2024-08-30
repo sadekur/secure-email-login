e2bf383df0c317b1e0c67e36bae61780c08a72ad
Sadekur <shadekur.rahman60@gmail.com>
2024-08-30 21:41:50 +0600

    complete

--- includes/Common.php
+++ includes/Common.php
@@ -4,9 +4,9 @@ namespace SecureEmailLogin\EmailLogin;
 
 class Common {
 	function __construct() {
-		add_action('login_form', [$this, 'custom_login_form_fields']);
-		add_action('login_footer', [$this, 'hidden_fields']);
-		add_filter('authenticate', [$this, 'email_login_authenticate'], 20, 3);
+		add_action( 'login_form', [ $this, 'custom_login_form_fields' ] );
+		add_action( 'login_footer', [ $this, 'hidden_fields' ] );
+		add_filter( 'authenticate', [ $this, 'email_login_authenticate' ], 20, 3 );
 	}
 
 	public function custom_login_form_fields() {
@@ -22,18 +22,16 @@ class Common {
 		<?php
 	}
 
-	public function email_login_authenticate($user, $username, $password) {
-		if (empty($username)) return;
-		if (!is_a($user, 'WP_User')) {
-			$user = get_user_by('email', $username);
+	public function email_login_authenticate( $user, $username, $password ) {
+		if ( empty($username ) ) return;
+		if ( !is_a( $user, 'WP_User' ) ) {
+			$user = get_user_by( 'email', $username );
 			if (!$user) {
-				return new \WP_Error('invalid_email', __('Invalid Email'));
+				return new \WP_Error( 'invalid_email', __( 'Invalid Email' ) );
 			}
 		}
 		return $user;
 	}
-	
-	
 
 	public function hidden_fields() {
 		?>
@@ -43,7 +41,6 @@ class Common {
 			<input type="text" id="otp" name="otp" placeholder="Enter OTP">
 			<button type="submit">Submit</button>
 		</form>
-
 		<?php
 	}
 }
--- includes/RestAPI.php
+++ includes/RestAPI.php
@@ -3,59 +3,59 @@ namespace SecureEmailLogin\EmailLogin;
 
 class RestAPI {
     public function __construct() {
-        add_action('rest_api_init', [$this, 'register_email_login_routes']);
-        add_action('rest_api_init', [$this, 'register_email_secureemaillogin']);
+        add_action( 'rest_api_init', [ $this, 'sel_register_email_login_routes' ] );
+        add_action( 'rest_api_init', [ $this, 'sel_register_email_secureemaillogin' ] );
     }
 
-    public function register_email_login_routes() {
+    public function sel_register_email_login_routes() {
         register_rest_route('secureemaillogin/v1', '/submit-email', [
-            'methods' => 'POST',
-            'callback' => [$this, 'handle_email_submission'],
-            'permission_callback' => '__return_true',
+            'methods'               => 'POST',
+            'callback'              => [$this, 'sel_handle_email_submission'],
+            'permission_callback'   => '__return_true',
         ]);
     }
 
-    public function register_email_secureemaillogin() {
+    public function sel_register_email_secureemaillogin() {
         register_rest_route('secureemaillogin/v1', '/verify-otp', [
-            'methods' => 'POST',
-            'callback' => [$this, 'handle_otp_verification'],
-            'permission_callback' => '__return_true',
+            'methods'               => 'POST',
+            'callback'              => [$this, 'sel_handle_otp_verification'],
+            'permission_callback'   => '__return_true',
         ]);
     }
 
-    public function handle_email_submission(\WP_REST_Request $request) {
-        $email = $request->get_param('email');
-        $user = get_user_by('email', $email);
-        if ($user) {
+    public function sel_handle_email_submission( \WP_REST_Request $request ) {
+        $email  = $request->get_param( 'email' );
+        $user   = get_user_by( 'email', $email );
+        if ( $user ) {
             wp_clear_auth_cookie();
             wp_set_current_user($user->ID);
             wp_set_auth_cookie($user->ID);
-            return new \WP_REST_Response(['userExists' => true], 200);
+            return new \WP_REST_Response( [ 'userExists' => true ], 200 );
         } else {
-            $otp = rand(100000, 999999);
-            set_transient('otp_' . $email, $otp, 10 * MINUTE_IN_SECONDS);
+            $otp = rand( 100000, 999999 );
+            set_transient( 'otp_' . $email, $otp, 10 * MINUTE_IN_SECONDS );
             $subject = "Your Login OTP";
             $message = "Here is your OTP for login: " . $otp;
-            $headers = ['Content-Type: text/html; charset=UTF-8'];
-            wp_mail($email, $subject, $message, $headers);
-            return new \WP_REST_Response(['userExists' => false], 200);
+            $headers = [ 'Content-Type: text/html; charset=UTF-8' ];
+            wp_mail( $email, $subject, $message, $headers );
+            return new \WP_REST_Response ([ 'userExists' => false ], 200 );
         }
     }
 
-    public function handle_otp_verification(\WP_REST_Request $request) {
-        $email = $request->get_param('email');
-        $name = $request->get_param('name');
-        $otp = $request->get_param('otp');
-        $stored_otp = get_transient('otp_' . $email);
-        if ($otp == $stored_otp) {
-            $user_id = wp_create_user($email, wp_generate_password(), $email);
-            wp_update_user(['ID' => $user_id, 'display_name' => $name]);
-            wp_set_current_user($user_id);
-            wp_set_auth_cookie($user_id);
-            delete_transient('otp_' . $email);
-            return new \WP_REST_Response(['success' => true], 200);
+    public function sel_handle_otp_verification( \WP_REST_Request $request ) {
+        $email      = $request->get_param( 'email' );
+        $name       = $request->get_param( 'name' );
+        $otp        = $request->get_param( 'otp' );
+        $stored_otp = get_transient( 'otp_' . $email );
+        if ( $otp == $stored_otp ) {
+            $user_id = wp_create_user( $email, wp_generate_password(), $email );
+            wp_update_user( [ 'ID' => $user_id, 'display_name' => $name ] );
+            wp_set_current_user( $user_id );
+            wp_set_auth_cookie( $user_id );
+            delete_transient( 'otp_' . $email );
+            return new \WP_REST_Response( [ 'success' => true ], 200 );
         } else {
-            return new \WP_REST_Response(['success' => false, 'message' => 'Invalid OTP'], 403);
+            return new \WP_REST_Response( [ 'success' => false, 'message' => 'Invalid OTP' ], 403);
         }
     }
 }
