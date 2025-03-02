de19b46cdfcef7478fe8df72d3dc080d0eaa4ad6
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-03-02 22:44:47 +0600

    update code

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'secureemaillogin/email-login',
+        'name' => 'secure-email-login/email-login',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '8b29fe4a78bc314ff8e7cd18c2cb804b74e0837a',
+        'reference' => 'b51b6b6dfac1fc6d728e8b7528c8c22d3c093367',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'secureemaillogin/email-login' => array(
+        'secure-email-login/email-login' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '8b29fe4a78bc314ff8e7cd18c2cb804b74e0837a',
+            'reference' => 'b51b6b6dfac1fc6d728e8b7528c8c22d3c093367',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
