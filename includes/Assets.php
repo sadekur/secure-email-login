8b29fe4a78bc314ff8e7cd18c2cb804b74e0837a
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-03-02 22:37:50 +0600

    update code

--- includes/Assets.php
+++ includes/Assets.php
@@ -61,25 +61,4 @@ class Assets {
 		wp_enqueue_script( 'email-common-script' );
 		wp_enqueue_style( 'email-login-style' );
 	}
-
-	// public function register_admin_assets() {
-	// 	$scripts 	= $this->get_scripts();
-	// 	$styles 	= $this->get_styles();
-
-	// 	wp_register_script( 'email-login-admin-script', $scripts[ 'email-login-admin-script' ][ 'src' ], $scripts['email-login-admin-script' ][ 'deps' ], $scripts[ 'email-login-admin-script' ][ 'version' ], true );
-		
-	// 	wp_localize_script( 'email-login-admin-script', 'email-login', [
-	// 		'nonce'   => wp_create_nonce( 'nonce' ),
-	// 		'confirm' => __( 'Are you sure?', 'email-login-crm' ),
-	// 		'ajaxurl' => admin_url( 'admin-ajax.php' ),
-	// 		'error'   => __( 'Something went wrong', 'email-login-crm' )
-	// 	]);
-
-	// 	wp_register_style( 'email-login-admin-style', $styles[ 'email-login-admin-style' ][ 'src' ], [], $styles['email-login-admin-style' ][ 'version' ]);
-	// 	wp_register_style( 'jquery-ui', $styles[ 'jquery-ui' ][ 'src' ], [], $styles[ 'jquery-ui' ][ 'version' ] );
-
-	// 	wp_enqueue_script( 'email-login-admin-script' );
-	// 	wp_enqueue_style( 'email-login-admin-style' );
-	// 	wp_enqueue_style( 'jquery-ui' );
-	// }
 }
